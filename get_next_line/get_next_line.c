a2da5bead17c1c0bdc96be296c8c22178d98d6dd
Diogo Martins Cruz Tavares <diogmart@c1r11s2.42porto.com>
2022-11-14 10:00:06 +0000

    first commit

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 12:29:05 by diogmart          #+#    #+#             */
+/*   Updated: 2022/11/14 09:49:24 by diogmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+/*
+	-Create a function that reads the next line of a file;
+	-You can use malloc(), read() and free();
+	-Repeated calls of the function should let you read
+	the text file pointed to by fd *One line at a time*;
+	-If there is nothing else to read or if an error occurred,
+	it should return NULL;
+*/
+
+char	*get_next_line(int fd)
+{
+	
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 12:29:09 by diogmart          #+#    #+#             */
+/*   Updated: 2022/11/11 12:35:20 by diogmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
